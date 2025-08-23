3b4033e06a8ec8e52f4cb8cacfc4316bfd44d360
Gabriel <gomesg827@gmail.com>
2025-08-23 12:10:58 -0300

    Criado middleware de autenticação.

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,28 @@
+import * as Errors from "../utils/errorHandler.js";
+import jwt from "jsonwebtoken";
+
+export function authMiddleware(req, res, next) {
+  try {
+    const tokenHeader = req.headers.authorization;
+
+    const token = tokenHeader && tokenHeader.split(" ")[1];
+
+    if (!token) {
+      throw new Errors.TokenError({
+        token: "Token não encontrado",
+      });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+      if (error) {
+        throw new Errors.TokenError({
+          token: "Token inválido",
+        });
+      }
+      req.user = decoded;
+      return next();
+    });
+  } catch (e) {
+    next(e);
+  }
+}
