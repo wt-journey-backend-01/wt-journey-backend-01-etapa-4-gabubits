07e07f4344cf612008f4ea12cf5b0b9ba25fd4d4
Gabriel <gomesg827@gmail.com>
2025-08-26 12:27:37 -0300

    Verificando erros

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,10 @@ import jwt from "jsonwebtoken";
 
 export function authMiddleware(req, res, next) {
   try {
-    console.log("Header recebido: ", req.headers.authorization);
+    console.log("Header recebido:\n", req.headers);
+    console.log("Body recebido:\n", req.body);
+    console.log("Method: ", req.method);
+    console.log("Route: ", req.path);
 
     const tokenHeader = req.headers.authorization;
 
