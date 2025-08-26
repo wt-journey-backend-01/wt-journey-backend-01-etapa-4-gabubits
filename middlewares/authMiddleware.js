1817ac1bd80def369e7784d40ca81ef1a764a997
Gabriel <gomesg827@gmail.com>
2025-08-26 12:15:34 -0300

    Verificando erros.

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -5,6 +5,8 @@ export function authMiddleware(req, res, next) {
   try {
     console.log("Header recebido:\n", req.headers);
     console.log("Body recebido:\n", req.body);
+    console.log("Method: ", req.method);
+    console.log("Route: ", req.path);
 
     const tokenHeader = req.headers.authorization;
 
